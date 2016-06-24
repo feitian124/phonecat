afe87beba80f3ccfd63babd9c1e00dd8ae0d0978
feitian124 <feitian124@qq.com>
2016-06-25 00:33:04 +0800

    order by price

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -9,19 +9,33 @@ angular.
       this.phones = [
         {
           name: 'Nexus S',
+          price: 2999,
           snippet: 'Fast just got faster with Nexus S.',
           age: 1
         }, {
           name: 'Motorola XOOM™ with Wi-Fi',
+          price: 3793,
           snippet: 'The Next, Next Generation tablet.',
           age: 2
         }, {
           name: 'MOTOROLA XOOM™',
+          price:  3197,
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3
+        }, {
+          name: 'MOTOROLA XOOM™',
+          price:  3295,
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3
+        }, {
+          name: 'MOTOROLA XOOM™',
+          price:  3591,
           snippet: 'The Next, Next Generation tablet.',
           age: 3
         }
       ];
 
       this.orderProp = 'age';
+      this.orderByPrice = 'price';
     }
   });
